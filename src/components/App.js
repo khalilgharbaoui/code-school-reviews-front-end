58e2b2b3c3becec42c93b315d083bfcca580017d
khalilgharbaoui <kaygeee@gmail.com>
2016-03-10 22:59:24 +0100

    installed the starter template and made an empty CodescoolList component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CodeschoolList from './CodeschoolList';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <CodeschoolList />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/CodeschoolList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CodeschoolList extends React.Component {
+
+  render() {
+    return (
+      <h1>code school list comes here</h1>
+    );
+  }
+}
+
+export default CodeschoolList;
