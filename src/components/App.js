6cbf2d57ecf7a15fa0012d42fc72244a2d3f8f1a
khalilgharbaoui <kaygeee@gmail.com>
2016-03-11 03:30:57 +0100

    added initial codeschool list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CodeschoolList from './CodeschoolList';
+import '../stylesheets/_bootstrap.scss';
+
 
 class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Hello World!</h1>
                 <CodeschoolList />
             </div>
--- src/components/CodeschoolList.js
+++ src/components/CodeschoolList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+require('../stylesheets/CodeschoolList.scss');
 
 class CodeschoolList extends React.Component {
 
@@ -15,12 +16,12 @@ class CodeschoolList extends React.Component {
 
 
     let compo = this;
-    let url = 'https://codeschoolreviews.herokuapp.com/';
+    let apiurl = 'https://codeschoolreviews.herokuapp.com/';
 
     //http://api.jquery.com/jQuery.ajax/
-    
+
     $.ajax({
-      url: url,
+      url: apiurl,
       dataType: 'json',
       contentType: 'application/json',
       method: 'GET'
@@ -29,18 +30,19 @@ class CodeschoolList extends React.Component {
     .success((response) => {
       console.log(response);
     })
-      .done((response) => {
-        compo.setState({
-          databucket: response.codeschools
-        });
-      })
-      .fail(function(data, jqXHR, textStatus){
-        // display the error message from the server... maybe
-        compo.setState({
-          errors: data.textStatus
+    .done((response) => {
+      compo.setState({
+        databucket: response.codeschools
       });
-      console.log(jqXHR);
-      });
+    })
+    .fail(function(data, jqXHR, textStatus){
+
+      compo.setState({
+          // display the error message from the server
+        errors: data.textStatus
+    });
+    console.log(jqXHR);
+    });
   }
 
   componentDidMount(){
@@ -48,11 +50,28 @@ class CodeschoolList extends React.Component {
   }
 
   render() {
-    return (
+    return (<div>
       <h1>
         code school list comes here
       </h1>
-    );
+
+      {this.state.databucket.map(function(codeschool){
+        let logolink = `https://codeschoolreviews.herokuapp.com${codeschool.logo.url}`
+        let starStyle = {
+            width: `${codeschool.average_rating * 20.0}%`
+          };
+        return(
+          <div key={codeschool.id}>
+            <p><a href={codeschool.url}><img src={logolink} /></a></p>
+            <h1>{codeschool.name}</h1><br />
+            <p><div className="star"><span style={starStyle} className="rating"></span></div></p>
+            <p>{codeschool.description}</p>
+            <p><a href={codeschool.url}>{codeschool.url}</a></p>
+          </div>
+        );
+      })}
+      
+    </div>);
   }
 }
 
