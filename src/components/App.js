e9a7bad3bb66dce5616b77e8bf9b0ca974270135
khalilgharbaoui <kaygeee@gmail.com>
2016-03-11 01:24:02 +0100

    fixed the cors settings

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CodeschoolList from './CodeschoolList';
 
 class App extends React.Component {
+
     render() {
         return (
             <div>
--- src/components/CodeschoolList.js
+++ src/components/CodeschoolList.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import $ from 'jquery';
 
 class CodeschoolList extends React.Component {
 
+  constructor(){
+    super();
+
+    this.state = {
+      databucket: []
+    }
+  }
+
+  getData(){
+
+
+    let compo = this;
+    let url = 'https://codeschoolreviews.herokuapp.com/';
+
+    //http://api.jquery.com/jQuery.ajax/
+    
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      contentType: 'application/json',
+      method: 'GET'
+
+    })
+    .success((response) => {
+      console.log(response);
+    })
+      .done((response) => {
+        compo.setState({
+          databucket: response.codeschools
+        });
+      })
+      .fail(function(data, jqXHR, textStatus){
+        // display the error message from the server... maybe
+        compo.setState({
+          errors: data.textStatus
+      });
+      console.log(jqXHR);
+      });
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
   render() {
     return (
-      <h1>code school list comes here</h1>
+      <h1>
+        code school list comes here
+      </h1>
     );
   }
 }
