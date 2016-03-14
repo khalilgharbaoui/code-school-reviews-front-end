5a79b942b2e0ca45caff7f95482e77dedb04592a
khalilgharbaoui <kaygeee@gmail.com>
2016-03-14 20:59:21 +0100

    minor css changes

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
                     path={location.pathname}
                     initialStyle={{opacity: 0.3, transform: 'translate(80%,0%) rotateY(0deg) skewY(0deg) scale(0.6)'}}
                     transition='opacity linear 2300ms, transform ease-in-out 1800ms'
-                    finalStyle={{opacity: 1, transform: 'translate(0%,0%) rotateY(-1440deg) skewY(-0.5deg) scale(1)'}}>
+                    finalStyle={{opacity: 1, transform: 'translate(0%,0%) rotateY(-1440deg) skewY(0deg) scale(1)'}}>
                     <div className='container'>
                         {this.props.children}
                     </div>
